531990b1eac6995fe1ee19e5992a6261b2cbfb47
liamnaka <nakagawaliam@gmail.com>
2019-12-01 12:42:39 -0800

    nit

--- model.py
+++ model.py
@@ -202,7 +202,7 @@ class ViewHoloGAN(HoloGAN):
                     errD_real = self.d_loss_real.eval(feed)
                     errG = self.g_loss.eval(feed)
                     errQ = self.q_loss.eval(feed)
-                    errPose - self.pose_loss.eval(feed)
+                    errPose = self.pose_loss.eval(feed)
                     print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f, q_loss: %.8f, pose_loss: %.8f" \
                         % (epoch, idx, batch_idxs,
                            time.time() - start_time, errD_fake + errD_real, errG, errQ, errPose))
